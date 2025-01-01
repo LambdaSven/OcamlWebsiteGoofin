d4eda3dee63104980457be963957a197f11f6cfa
Sven Null <mail@sasmith.ca>
2025-01-01 17:57:06 -0400

    first commit - we have counting.

---
+++ README.md
@@ -0,0 +1,16 @@
+This project should be run in a switch with ocaml 5.1.0, using 
+
+`opam switch create . 5.1.0`
+
+This project requires the following dependencies
+
+Dream
+Tyxml
+
+Additionally, if you're working on it you should also install 
+
+ocaml-lsp-server
+
+You can instal all of the above with the following command.
+
+`opam install dream tyxml ocaml-lsp-server`
---
+++ bin/main.ml
@@ -0,0 +1,77 @@
+let number = ref 0
+
+let l_link = 
+  let open Tyxml.Html in 
+   a ~a:[a_href "/ily"] [txt "Hey, Guess What?"]
+
+let htmx_script = 
+  let open Tyxml.Html in
+  script ~a:[a_src "https://unpkg.com/htmx.org@2.0.4"] (txt "")
+
+let ht_button_Increase =
+  let open Tyxml.Html in 
+  button ~a:[
+    Unsafe.string_attrib "hx-post" "/inc";
+    Unsafe.string_attrib "hx-target" "#count";
+    Unsafe.string_attrib "hx-swap" "textContent"]
+  [txt "Increase"] 
+
+
+let ht_button_decrease =
+  let open Tyxml.Html in 
+  button ~a:[
+    Unsafe.string_attrib "hx-post" "/dec";
+    Unsafe.string_attrib "hx-target" "#count";
+    Unsafe.string_attrib "hx-swap" "textContent"] 
+  [txt "Decrease"] 
+
+let count = 
+  let open Tyxml.Html in
+  p ~a:[a_id "count"] [txt "0"]
+
+let counter = 
+  let open Tyxml.Html in 
+  div ~a:[a_class ["counter"]]
+  [ht_button_Increase; count; ht_button_decrease]
+
+let page = 
+  let open Tyxml.Html in 
+  html 
+    (head (title (txt "Wheeee!")) [htmx_script])
+    (body [l_link; counter])
+
+(*
+let html_to_string html = Fmt.str "%a" (Tyxml.Html.pp ()) html
+*)
+
+let elt_to_string elt = Fmt.str "%a" (Tyxml.Html.pp_elt ()) elt
+
+
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router [
+    Dream.get "/" (
+      fun _ -> 
+       Dream.html @@ elt_to_string page
+    );
+    Dream.get "/echo/:word" (
+      fun request -> 
+        Dream.html @@ Dream.param request "word"
+      );
+    Dream.get "/ily" (
+      fun _ -> 
+        Dream.html "I love you :)"
+    );
+    Dream.post "/inc" (
+      fun _ -> 
+      number := !number + 1;
+        Dream.html @@ string_of_int !number
+      );
+    Dream.post "/dec" (
+      fun _ -> 
+      number := !number - 1;
+      Dream.html @@ string_of_int !number
+      )
+  ]
