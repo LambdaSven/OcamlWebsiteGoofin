8e94022b5224140800de496312457aee3c8c6186
Sven Null <mail@sasmith.ca>
2025-01-02 12:01:36 -0400

    hacky, syncs with multiple clients now

--- bin/main.ml
+++ bin/main.ml
@@ -25,14 +25,24 @@ let ht_button_decrease =
     Unsafe.string_attrib "hx-swap" "textContent"] 
   [txt "Decrease"] 
 
+let ht_button_update =
+  let open Tyxml.Html in
+  button ~a:[
+    Unsafe.string_attrib "hx-get" "/update";
+    Unsafe.string_attrib "hx-trigger" "load, every 1s";
+    Unsafe.string_attrib "hx-target" "#count";
+    Unsafe.string_attrib "hx-swap" "textContent"
+  ]
+    [txt "Update!"]
+
 let count = 
   let open Tyxml.Html in
-  p ~a:[a_id "count"] [txt "0"]
+  p ~a:[a_id "count"] [txt @@ string_of_int !number]
 
 let counter = 
   let open Tyxml.Html in 
   div ~a:[a_class ["counter"]]
-  [ht_button_Increase; count; ht_button_decrease]
+    [ht_button_Increase; count; ht_button_decrease; ht_button_update]
 
 let page = 
   let open Tyxml.Html in 
@@ -49,12 +59,12 @@ let elt_to_string elt = Fmt.str "%a" (Tyxml.Html.pp_elt ()) elt
 
 
 let () =
-  Dream.run
+  Dream.run ~interface:"192.168.0.106"
   @@ Dream.logger
   @@ Dream.router [
     Dream.get "/" (
-      fun _ -> 
-       Dream.html @@ elt_to_string page
+      fun _ ->
+      Dream.html @@ elt_to_string page
     );
     Dream.get "/echo/:word" (
       fun request -> 
@@ -73,5 +83,8 @@ let () =
       fun _ -> 
       number := !number - 1;
       Dream.html @@ string_of_int !number
+    );
+    Dream.get "/update" (
+      fun _ -> Dream.html @@ string_of_int !number
       )
   ]
